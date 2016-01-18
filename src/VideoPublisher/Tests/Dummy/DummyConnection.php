abc636e58c3a183fc9d5a45baede48f876ff01e7
Bart Malestein <bart@isset.nl>
2016-01-18 13:30:45 +0100

    - cleaned up DummyConnection

--- src/VideoPublisher/Tests/Dummy/DummyConnection.php
+++ src/VideoPublisher/Tests/Dummy/DummyConnection.php
@@ -18,47 +18,60 @@ class DummyConnection implements ConnectionInterface
     {
         switch ($payload->getUrl()) {
             case 'http://my.videopublisher.io/api/published':
-                $streams = [];
-                $streams[] = [
-                    'streamName' => 'test',
-                    'uuid' => '00000000-0000-0000-0000-00000000',
-                    'status' => 'mock',
-                    'enabled' => true
-                ];
-                $streams[] = [
-                    'streamName' => 'test',
-                    'uuid' => '00000000-0000-0000-0000-00000000',
-                    'status' => 'mock',
-                    'enabled' => true
-                ];
-                $streams[] = [
-                    'streamName' => 'test',
-                    'uuid' => '00000000-0000-0000-0000-00000000',
-                    'status' => 'mock',
-                    'enabled' => true
-                ];
-
-                return new CurlResponse(200, null, json_encode($streams));
+                return $this->generateListStreamsResponse();
             case 'http://my.videopublisher.io/api/publish/00000000-0000-0000-0000-00000000':
-                $stream = [
-                    'streamName' => 'test',
-                    'uuid' => '00000000-0000-0000-0000-00000000',
-                    'status' => 'mock',
-                    'enabled' => true,
-                    'viewable' => true,
-                    'view' => [
-                        'playout_url' => 'http://playout.com/video.mp4',
-                        'video_player' => 'http://playout.com/player/video.mp4'
-                    ]
-                ];
-                return new CurlResponse(200, null, json_encode($stream));
+                return $this->generateGetStreamResponse();
             case 'http://my.videopublisher.io/exception/api/published':
                 return new CurlResponse(400, null, null);
             case 'http://my.videopublisher.io/exception/api/publish/00000000-0000-0000-0000-00000000':
                 return new CurlResponse(400, null, null);
         }
+    }
 
-        var_dump($payload->getUrl());
-        die;
+    /**
+     * @return CurlResponse
+     */
+    private function generateListStreamsResponse()
+    {
+        $streams = [];
+        $streams[] = [
+            'streamName' => 'test',
+            'uuid' => '00000000-0000-0000-0000-00000000',
+            'status' => 'mock',
+            'enabled' => true
+        ];
+        $streams[] = [
+            'streamName' => 'test',
+            'uuid' => '00000000-0000-0000-0000-00000000',
+            'status' => 'mock',
+            'enabled' => true
+        ];
+        $streams[] = [
+            'streamName' => 'test',
+            'uuid' => '00000000-0000-0000-0000-00000000',
+            'status' => 'mock',
+            'enabled' => true
+        ];
+
+        return new CurlResponse(200, null, json_encode($streams));
+    }
+
+    /**
+     * @return CurlResponse
+     */
+    private function generateGetStreamResponse()
+    {
+        $stream = [
+            'streamName' => 'test',
+            'uuid' => '00000000-0000-0000-0000-00000000',
+            'status' => 'mock',
+            'enabled' => true,
+            'viewable' => true,
+            'view' => [
+                'playout_url' => 'http://playout.com/video.mp4',
+                'video_player' => 'http://playout.com/player/video.mp4'
+            ]
+        ];
+        return new CurlResponse(200, null, json_encode($stream));
     }
 }
